de3860c5b38dcaab2b369ad4f4029513240a7d98
Ekaterina Schedlberger <ek.schedl@gmail.com>
2024-03-25 22:36:25 +0100

    + namePerson done

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-const lang = prompt("Выберите язык", "например: ru или eng");
-const text_eng =
+const lang = prompt("Выберите язык", "например: ru или en");
+const text_en =
   "Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday";
 const text_ru =
   "Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Восресенье";
 if (lang == "ru") {
   console.log(text_ru);
-} else if (lang == "eng") {
-  console.log(text_eng);
+} else if (lang == "en") {
+  console.log(text_en);
 } else {
-  console.log("Выберите язык, например: ru или eng");
+  console.log("Выберите язык, например: ru или en");
 }
 
 switch (lang) {
   case "ru":
     console.log(text_ru);
     break;
-  case "eng":
-    console.log(text_eng);
+  case "en":
+    console.log(text_en);
     break;
   default:
-    console.log("Выберите язык, например: ru или eng");
+    console.log("Выберите язык, например: ru или en");
     break;
 }
 // честно признаюсь /этот вариант я гуглила, сама я такео еще не знаю, но логика понятна
@@ -48,3 +48,12 @@ const daysOfWeek = {
 
 const result = daysOfWeek[lang2] || "Выберите язык rus или eng";
 console.log(result);
+
+let namePerson = prompt("Выберите имя", "Артем, Александр, другое");
+const role =
+  namePerson === "Артем"
+    ? "директор"
+    : namePerson === "Александр"
+    ? "преподаватель"
+    : "студент";
+console.log(role);
