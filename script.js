7da507afdfc6846c74ee85aeab61005ed0292612
Ekaterina Schedlberger <ek.schedl@gmail.com>
2024-03-30 00:15:23 +0100

    lesson07 done

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 // waiting for feedback from teacher
+
+"use strict";
+
+let week = [
+  "Понедельник",
+  "Вторник",
+  "Среда",
+  "Четверг",
+  "Пятница",
+  "Суббота",
+  "Воскресенье",
+];
+
+for (let i = 0; i < week.length; i++) {
+  console.log(week[i]);
+}
+
+console.log(week);
+
+let currentDate = new Date();
+let currentDay = currentDate.getDay();
+
+for (let i = 0; i < week.length; i++) {
+  if (i === currentDay) {
+    console.log(`%c${week[i]}`, "font-weight: bold");
+  } else if (i === 5 || i === 6) {
+    console.log(`%c${week[i]}`, "font-style: italic");
+  } else {
+    console.log(week[i]);
+  }
+}
